7165506ef46550cad00d14aa718321cb661c571f
Kryosen <gonuri91@hotmail.com>
2022-05-03 15:02:53 +0700

    Header completado

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,33 @@
+import logoPagina from "./logoPagina.jpg";
+import "./NavBar.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="contenedorHeader">
+        <img src={logoPagina} className="logo" alt="logo" />
+        <nav className="navBar">
+          <ul className="navBarList">
+            <li className="navBarItem">
+              <a href="">Home</a>
+            </li>
+            <li className="navBarItem">
+              <a href="">Envios</a>
+            </li>
+            <li className="navBarItem">
+              <a href="">Mis Envios</a>
+            </li>
+            <li className="navBarItem">
+              <a href="">Precios</a>
+            </li>
+            <li className="navBarItem">
+              <a href="">Contacto</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
